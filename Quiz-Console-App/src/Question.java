cdf4a8202b0dc0110094ab3a76dfd996146780ff
Vladyslav Kokhan <vladislav27ko@gmail.com>
2025-03-06 15:35:59 +0200

    Second commit for Quiz-Console-App create questions and display them

--- Quiz-Console-App/src/Question.java
+++ Quiz-Console-App/src/Question.java
@@ -1,11 +1,22 @@
 public class Question {
     private int id;
     private String question;
-    private String answer;
     private String opt1;
     private String opt2;
     private String opt3;
     private String opt4;
+    private String answer;
+
+    public Question(int id, String question, String opt1, String opt2, String opt3, String opt4, String answer) {
+
+        this.id = id;
+        this.question = question;
+        this.opt1 = opt1;
+        this.opt2 = opt2;
+        this.opt3 = opt3;
+        this.opt4 = opt4;
+        this.answer = answer;
+    }
 
     public String getOpt4() {
         return opt4;
@@ -61,5 +72,18 @@ public class Question {
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Question{" +
+                "id=" + id +
+                ", question='" + question + '\'' +
+                ", opt1='" + opt1 + '\'' +
+                ", opt2='" + opt2 + '\'' +
+                ", opt3='" + opt3 + '\'' +
+                ", opt4='" + opt4 + '\'' +
+                ", answer='" + answer + '\'' +
+                '}';
     }
 }
--- Quiz-Console-App/src/QuestionService.java
+++ Quiz-Console-App/src/QuestionService.java
@@ -1,5 +1,19 @@
 public class QuestionService {
     Question[] questions = new Question[5];
 
-    
+    public QuestionService() {
+        questions[0] = new Question(1,"size of int", "2" ,"6" , "8", "4", "4");
+        questions[1] = new Question(2, "What is the capital of France?", "Berlin", "Madrid", "Paris", "Rome", "Paris");
+        questions[2] = new Question(3, "Which planet is known as the Red Planet?", "Earth", "Venus", "Jupiter", "Mars", "Mars");
+        questions[3] = new Question(4, "How many continents are there on Earth?", "5", "6", "7", "8", "7");
+        questions[4] = new Question(5, "What is the result of 5 + 3 * 2?", "11", "16", "10", "8", "11");
+
+    }
+
+    public void displayQuestions() {
+       for (Question q : questions){
+           System.out.println("Question "+ q);
+       }
+
+    }
 }
